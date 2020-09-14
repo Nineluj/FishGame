2ef143e717b1a7d3f5336c10cb507fcd3d349127
Julian Hirn <julian.hirn@protonmail.com>
2020-09-14 11:31:36 -0400

    added simple tests for xyes

--- B/Other/index.test.ts
+++ B/Other/index.test.ts
@@ -1,16 +1,73 @@
 import { expect } from "chai"
-import { main } from "./index"
+import { xyes, Writer } from "./index"
 
-describe("it returns true", () => {
-    it("returns true", () => {
-        expect(true).to.equal(true)
+const xyesAsync = async (args: Array<string>, writer: Writer) : Promise<void> => {
+    xyes(args, writer)
+    return Promise.resolve()
+}
+
+describe("xyes", () => {
+    let state: Array<string> = []
+
+    let testWriter: Writer = {
+        write: arg0 => {
+            state.push(arg0)
+        }
+    }
+
+    const resetState = () => {
+        state = []
+    }
+
+    beforeEach(() => {
+        resetState()
     })
-})
 
-describe("test2", () => {
-    it("..", () => {
-        main()
+    it("prints hello world 20 times when passed only limit", () => {
+        xyes(["-limit"], testWriter)
+        expect(state).to.have.lengthOf(20)
+
+        state.forEach(value => {
+            expect(value).equal("hello world")
+        })
     })
-})
 
+    it("prints given string 20 times when passed only limit", () => {
+        const customArgs = ["matthias", "will", "wreck", "our", "code"]
+        const shouldBePrinting = "matthias will wreck our code"
+        xyes(["-limit", ...customArgs], testWriter)
+        expect(state).to.have.lengthOf(20)
+
+        state.forEach(value => {
+            expect(value).equal(shouldBePrinting)
+        })
+    })
+
+    const specialWords = ["null", "undefined", ""]
+    specialWords.forEach(str => {
+        it(`prints ${str} 20 times when passed limit and ${str}`, () => {
+            xyes(["-limit", str], testWriter)
+            expect(state).to.have.lengthOf(20)
+
+            state.forEach(value => {
+                expect(value).equal(str)
+            })
+        })
+    })
+
+    // it("the output grows over time when not passed limit", () => {
+    //     setTimeout(() => {
+    //         xyes([], testWriter)
+    //     }, 100)
+    //     let shortRunOutput = [...state]
+    //     resetState()
+    //
+    //     setTimeout(() => {
+    //         xyes([], testWriter)
+    //     }, 500)
+    //     let longRunOutput = [...state]
+    //
+    //     expect(shortRunOutput).to.have.lengthOf.at.least(longRunOutput.length)
+    // })
+})
 
--- B/Other/index.ts
+++ B/Other/index.ts
@@ -1,3 +1,7 @@
+export interface Writer {
+    write: (arg0: string) => void
+}
+
 type Limit = number | "inf"
 const limitFlagValue = 20
 
@@ -5,21 +9,21 @@ const limitFlagValue = 20
  * Prints the message up to limit times or forever if it's infinite
  * @param message The message to print to STDOUT
  * @param limit Number of times to print
+ * @param writer Where to output the string to
  */
-const printMessage = (message: string, limit: Limit) : void => {
+const printMessage = (message: string, limit: Limit, writer: Writer) : void => {
     for (let i = 0; limit == "inf" || i !== limit; i++) {
-        console.log(message)
+        writer.write(message)
     }
 }
 
 /**
  * Reads input from argv and returns the message to print and the limit
  */
-const parseInput = () : {message : string, limit: Limit} => {
+const parseInput = (args: Array<string>) : {message : string, limit: Limit} => {
     let message = "hello world"
     let limit: Limit = "inf"
 
-    let args = process.argv.slice(2)
     let nArgs = args.length
 
     if (nArgs > 0) {
@@ -27,19 +31,33 @@ const parseInput = () : {message : string, limit: Limit} => {
             limit = limitFlagValue
 
             if (nArgs > 1) {
-                message = args[1]
+                // use slice to remove the -limit flag
+                message = args.slice(1).join(" ")
             }
         } else {
-            message = args[0]
+            message = args.join(" ")
         }
     }
 
     return {message, limit}
 }
 
-export const main = () : void => {
-    const { message, limit } = parseInput()
-    printMessage(message, limit)
+/**
+ * Writes the arguments to the writer infinite number of times unless the first argument is limit
+ * @param args What to print out, first item can be "-limit" to only print 20 times
+ * @param writer The location where we write to
+ */
+export const xyes = (args: Array<string>, writer: Writer) : void => {
+    const { message, limit } = parseInput(args)
+    printMessage(message, limit, writer)
 }
 
-main()
+if (require.main == module) {
+    const consoleWriter = {
+        write: (arg0: string) : void => {
+            console.log(arg0)
+        }
+    }
+    // slice args here since first two args are node and file being run
+    xyes(process.argv.slice(2), consoleWriter)
+}
