4df1123591b2c5d3d7d15364785200ecc14796f6
Julian Hirn <julian.hirn@protonmail.com>
2020-10-05 14:10:47 -0400

    started adding types

---
+++ Fish/Common/src/renderer/board.tsx
@@ -0,0 +1,3 @@
+import { Tile } from "@/renderer/tile"
+
+type Board = Array<Array<Tile>>
---
+++ Fish/Common/src/renderer/tile.ts
@@ -0,0 +1,9 @@
+// Stub interface
+interface Player {}
+
+interface Tile {
+    fish: number
+    player?: Player
+}
+
+export { Tile }
