ceb997b656a5be3ad9d9a23fd815bd9d4c4d56d5
Julian Hirn <julian.hirn@protonmail.com>
2020-10-29 11:18:41 -0400

    started writing the complicated strategies

--- Fish/Player/src/strategy/strategy.ts
+++ Fish/Player/src/strategy/strategy.ts
@@ -13,7 +13,11 @@ import {
     getCoordinatesOfNextUnoccupiedTileToTheRight,
 } from "../../../Common/src/models/board/board"
 import { Point } from "../../../Common/src/models/point"
-import { createGameNode } from "../../../Common/src/models/tree/tree"
+import {
+    applyToAllFutureStates,
+    completeAction,
+    createGameNode,
+} from "../../../Common/src/models/tree/tree"
 
 /**
  * A Strategy is a function object that returns a suggested action that
@@ -81,45 +85,120 @@ const getPenguinPlacementStrategy = (fallbackStrategy: Strategy): Strategy => {
     }
 }
 
-// /**
-//  * Creates a strategy which minimizes the target's score the next time they
-//  * play their turn
-//  */
-// const getFuckOverPlayerPenguinMoveStrategy = (
-//     stepsAhead: number,
-//     targetId: string,
-//     fallbackStrategy: Strategy
-// ): Strategy => ({
-//     getNextAction: (gs: GameState): Action => {
-//         if (gs.phase !== "playing") {
-//             return fallbackStrategy.getNextAction(gs)
-//         }
+/**
+ * Creates a strategy which minimizes the target's score the next time they
+ * play their turn
+ */
+const getFuckOverPlayerPenguinMoveStrategy = (
+    targetId: string,
+    fallbackStrategy: Strategy
+): Strategy => ({
+    getNextAction: (gs: GameState): Action => {
+        if (gs.phase !== "playing") {
+            return fallbackStrategy.getNextAction(gs)
+        }
 
-//         let { player } = getPlayerWhoseTurnItIs(gs)
-//         const tree = createGameNode(gs)
+        throw new Error("not implemented")
+    },
+})
 
-//         const possibleMoves = tree.children()
+/**
+ * Picks the move that has the lowest row number for the place from which the penguin is moved
+ * and within this row, the lowest column number. Same process occurs on the destinations
+ * if two or more moves have the same origin
+ * @param moves moveActions to evaluate
+ */
+const tiebreakMoves = (moves: Array<Action>): Action => {
+    // TODO: finish this
+    let best = {}
 
-//         return ...
-//     },
-// })
+    for (let m of moves) {
+        if (m.data.actionType !== undefined && m.data.actionType === "move") {
+        }
+    }
 
-// /**
-//  * Creates a strategy which realizes the best gain after looking ahead stepsAhead moves
-//  */
-// const getPenguinMaxMinMoveStrategy = (
-//     stepsAhead: number,
-//     fallbackStrategy: Strategy
-// ): Strategy => ({
-//     getNextAction: (gs: GameState): Action => {
-//         if (gs.phase !== "playing") {
-//             return fallbackStrategy.getNextAction(gs)
-//         }
+    throw new Error("not implemented")
+}
 
-//         let { player } = getPlayerWhoseTurnItIs(gs)
+/**
+ * Gets the
+ */
+const getBestMovesOneTurn = (gn: GameNode): Array<Action> => {
+    //    applyToAllFutureStates(gn, (gs: GameState) => {})
 
-//         const tree = createGameNode(gs)
-//         tree.children()
-//         return ...
-//     },
-// })
+    throw new Error("not implemented")
+}
+
+/**
+ * Creates a strategy which realizes the best gain after looking ahead stepsAhead moves
+ */
+const getPenguinMaxMinMoveStrategy = (
+    stepsAhead: number,
+    fallbackStrategy: Strategy
+): Strategy => ({
+    getNextAction: (gs: GameState): Action => {
+        if (gs.phase !== "playing") {
+            return fallbackStrategy.getNextAction(gs)
+        }
+
+        let { player } = getPlayerWhoseTurnItIs(gs)
+
+        const tree = createGameNode(gs)
+        if (stepsAhead === 0) {
+            // pick the one that gets the most point in one move
+            return tiebreakMoves(getBestMovesOneTurn(tree))
+        }
+
+        /* assess and collect the best possible moves that the player can make */
+        const possibleFutures = tree.children()
+
+        let best = {
+            // score that can be achieved in that subtree
+            scoreAchieved: player.score,
+            // initial action taken to get into that subtree
+            // (multiple if they have the same max score)
+            actions: [createSkipTurnAction(player.id)],
+        }
+
+        for (let future of possibleFutures) {
+            let node = future
+
+            for (let i = 0; i < stepsAhead * gs.players.length; i++) {
+                // TODO: -1?
+                let newAction
+
+                if (getPlayerWhoseTurnItIs(node.gs).player.id === player.id) {
+                    newAction = getPenguinMaxMinMoveStrategy(
+                        stepsAhead - 1,
+                        getSkipTurnStrategy()
+                    )
+                } else {
+                    newAction = getFuckOverPlayerPenguinMoveStrategy(
+                        player.id,
+                        getSkipTurnStrategy()
+                    )
+                }
+
+                node = completeAction(node, newAction.getNextAction(node.gs))
+            }
+
+            // find the score
+            let score = 0 // TODO
+
+            // compare the score against the other possibilities
+            if (score === best.scoreAchieved) {
+                best.actions.push(future.action)
+            } else if (score > best.scoreAchieved) {
+                best.scoreAchieved = score
+                best.actions = [future.action]
+            }
+        }
+
+        if (best.actions.length === 1) {
+            return best.actions[0]
+        }
+
+        // tie breaker
+        return tiebreakMoves(best.actions)
+    },
+})
