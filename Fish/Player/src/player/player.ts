db9ceda350c153983bb21b13f68c7a5d21e2e406
Julian Hirn <julian.hirn@protonmail.com>
2020-11-05 22:10:46 -0500

    wrote jsdoc for player interface and player class

--- Fish/Player/src/player/player.ts
+++ Fish/Player/src/player/player.ts
@@ -11,12 +11,18 @@ import { Referee } from "../../../Admin/src/referee/referee"
 const DEFAULT_MOVES_AHEAD = 2
 
 /**
- * TODO: write JSDoc
+ * Object that uses the minMax move and the zig zag placement
+ * strategies to communicate with the referee to play a game of Fish
  */
 export class Player implements PlayerInterface {
     private strategy: Strategy
     private referee: Referee
 
+    /**
+     * @param referee is the referee that the player interacts with
+     * to make actions. The referee is tasked with calling the methods
+     * on this player to give it information about the game
+     */
     constructor(referee: Referee) {
         this.strategy = getPenguinMaxMinMoveStrategy(
             DEFAULT_MOVES_AHEAD,
